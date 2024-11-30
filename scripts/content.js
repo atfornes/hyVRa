25360787ec6cb04e3d15bc4b4d83fb38ff4af013
Ámbar Tenorio Fornés <ambar@atenor.io>
2024-11-30 13:34:27 +0100

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,55 @@
+// HyVRa: VR hydra extension
+let hyvra = {
+	VRdistance: 0.641,
+	adjustingVR: true,
+	alpha: 0,
+	beta: 0,
+	gamma: 0
+};
+let v = window.document.getElementById("hyVRa");
+if (!v) {
+	if (window.DeviceOrientationEvent) {
+		window.addEventListener('deviceorientation', function(event) {
+			hyvra.alpha = event.alpha;
+			hyvra.beta = event.beta;
+			hyvra.gamma = event.gamma;
+			if (hyvra.adjustingVR) {
+				hyvra.VRdistance += event.beta * 0.02;
+			}
+		});
+	}
+
+	vrWrapper = document.createElement("div");
+	vrWrapper.id = "hyVRa";
+	vrWrapper.addEventListener("click", function(element) {
+		hyvra.adjustingVR = !hyvra.adjustingVR;
+	});
+
+
+	vr = document.createElement("i");
+	vr.id = "hyvra-icon";
+	vr.id = "vr-icon";
+	vr.title = "VR adjust";
+	vr.className = "fas icon fa-glasses";
+	vr.ariaHidden = true;
+	vrWrapper.appendChild(vr);
+	document.body.appendChild(vrWrapper);
+}
+
+
+	vrWrapper = document.createElement("div");
+	vrWrapper.id = "hyVRa";
+	vrWrapper.addEventListener("click", function(element) {
+		hyvra.adjustingVR = !hyvra.adjustingVR;
+	});
+
+
+	vr = document.createElement("i");
+	vr.id = "hyvra-icon";
+	vr.id = "vr-icon";
+	vr.title = "VR adjust";
+	vr.className = "fas icon fa-glasses";
+	vr.ariaHidden = true;
+	vrWrapper.appendChild(vr);
+	document.body.appendChild(vrWrapper);
+	console.log(vrWrapper);
